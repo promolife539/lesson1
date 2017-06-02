9185354008e446f11c2f8f1f5f512d018ecb06d6
promolife539 <promolife539@mail.ru>
2017-06-02 23:13:55 +0600

    Add files via upload

---
+++ if1.py
@@ -0,0 +1,9 @@
+x=int(input('your age?'))
+if x<7:
+    print('kindergarten')
+elif x>=7 and x<=18:
+    print('school')
+elif x>18 and x<=24:
+    print('univercity')
+else:
+    print('go work hard')
---
+++ if2.py
@@ -0,0 +1,8 @@
+x=input()
+y=input()
+if len(x)==len(y):
+    print(1)
+elif x != y and len(x)>len(y):
+    print (2)
+elif x != y and y == ('learn'):
+    print(3)
